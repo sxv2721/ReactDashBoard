f1a68a6573b8f3c710a6f4be52f5150580956f01
Tandemseven <tandemseven@Note285.local>
2019-09-17 15:41:47 -0400

    hopefully restoring everythin

---
+++ precache-manifest.07c4f8ca0817a2452d8b942b78e153f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c3b7e728f7b8d70d102b72c5d02730",
+    "url": "/ReactDashBoard/index.html"
+  },
+  {
+    "revision": "d9b5209d523e61cd93e4",
+    "url": "/ReactDashBoard/static/css/main.d976af95.chunk.css"
+  },
+  {
+    "revision": "ca849b72a6e743d45343",
+    "url": "/ReactDashBoard/static/js/2.6de7e433.chunk.js"
+  },
+  {
+    "revision": "d9b5209d523e61cd93e4",
+    "url": "/ReactDashBoard/static/js/main.fe88dbe0.chunk.js"
+  },
+  {
+    "revision": "f9381d70a4968563d3a2",
+    "url": "/ReactDashBoard/static/js/runtime~main.09a3fc01.js"
+  },
+  {
+    "revision": "609225110ea93d6fac5d54bee2a0c122",
+    "url": "/ReactDashBoard/static/media/highcharts-compare-data-using-column-chart.60922511.png"
+  }
+]);
