397d0a8f14a273a92af34b3480d7d476db489b7b
Tandemseven <tandemseven@Note285.local>
2019-09-17 13:54:34 -0400

    Updates

---
+++ precache-manifest.63f4a5cf83dea7bd09a5a0b780406931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6e1edea6bfc5a6b832361aa48ffb46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a574c959d5b2330cfaaf",
+    "url": "/static/css/main.d976af95.chunk.css"
+  },
+  {
+    "revision": "ca849b72a6e743d45343",
+    "url": "/static/js/2.6de7e433.chunk.js"
+  },
+  {
+    "revision": "a574c959d5b2330cfaaf",
+    "url": "/static/js/main.ffbe5160.chunk.js"
+  },
+  {
+    "revision": "cb4fd463e7184abd8f6a",
+    "url": "/static/js/runtime~main.d52357dd.js"
+  },
+  {
+    "revision": "609225110ea93d6fac5d54bee2a0c122",
+    "url": "/static/media/highcharts-compare-data-using-column-chart.60922511.png"
+  }
+]);
